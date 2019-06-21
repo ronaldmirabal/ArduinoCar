70aa6a171acefe8a05e6ddfec75165df1ce4d732
ronald mirabal <ronaldmirabal@outlook.es>
2019-06-21 08:41:15 -0400

    upload

---
+++ ArduinoCar/ArduinoCar/MainActivity.cs
@@ -0,0 +1,187 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Bluetooth;
+using System.IO;
+using Java.Util;
+using System;
+using System.Threading.Tasks;
+
+namespace ArduinoCar
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+
+
+
+        //Creamos las variables necesarios para trabajar
+        //Widgets
+        ToggleButton tgConnect;
+        TextView Result;
+        //String a enviar
+        private Java.Lang.String dataToSend;
+        //Variables para el manejo del bluetooth Adaptador y Socket
+        private BluetoothAdapter mBluetoothAdapter = null;
+        private BluetoothSocket btSocket = null;
+        //Streams de lectura I/O
+        private Stream outStream = null;
+        private Stream inStream = null;
+        //MAC Address del dispositivo Bluetooth
+        private static string address = "98:D3:31:F5:33:60";
+        //Id Unico de comunicacion
+        private static UUID MY_UUID = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
+
+
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            //Asignacion de widgets
+            tgConnect = FindViewById<ToggleButton>(Resource.Id.toggleButton1);
+            //Asignacion de evento del toggle button
+            tgConnect.CheckedChange += tgConnect_HandleCheckedChange;
+            //Verificamos la disponibilidad del sensor Bluetooth en el dispositivo
+            CheckBt();
+        }
+
+
+        //Metodo de verificacion del sensor Bluetooth
+        private void CheckBt()
+        {
+            //asignamos el sensor bluetooth con el que vamos a trabajar
+            mBluetoothAdapter = BluetoothAdapter.DefaultAdapter;
+
+            //Verificamos que este habilitado
+            if (!mBluetoothAdapter.Enable())
+            {
+                Toast.MakeText(this, "Bluetooth Desactivado",
+                    ToastLength.Short).Show();
+            }
+            //verificamos que no sea nulo el sensor
+            if (mBluetoothAdapter == null)
+            {
+                Toast.MakeText(this,
+                    "Bluetooth No Existe o esta Ocupado", ToastLength.Short)
+                    .Show();
+            }
+        }
+
+        //Evento de cambio de estado del toggle button
+        void tgConnect_HandleCheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
+        {
+            if (e.IsChecked)
+            {
+                //si se activa el toggle button se incial el metodo de conexion
+                Connect();
+            }
+            else
+            {
+                //en caso de desactivar el toggle button se desconecta del arduino
+                if (btSocket.IsConnected)
+                {
+                    try
+                    {
+                        btSocket.Close();
+                    }
+                    catch (System.Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        //Evento de conexion al Bluetooth
+        public void Connect()
+        {
+            //Iniciamos la conexion con el arduino
+            BluetoothDevice device = mBluetoothAdapter.GetRemoteDevice(address);
+            System.Console.WriteLine("Conexion en curso" + device);
+
+            //Indicamos al adaptador que ya no sea visible
+            mBluetoothAdapter.CancelDiscovery();
+            try
+            {
+                //Inicamos el socket de comunicacion con el arduino
+                btSocket = device.CreateRfcommSocketToServiceRecord(MY_UUID);
+                //Conectamos el socket
+                btSocket.Connect();
+                System.Console.WriteLine("Conexion Correcta");
+            }
+            catch (System.Exception e)
+            {
+                //en caso de generarnos error cerramos el socket
+                Console.WriteLine(e.Message);
+                try
+                {
+                    btSocket.Close();
+                }
+                catch (System.Exception)
+                {
+                    System.Console.WriteLine("Imposible Conectar");
+                }
+                System.Console.WriteLine("Socket Creado");
+            }
+            //Una vez conectados al bluetooth mandamos llamar el metodo que generara el hilo
+            //que recibira los datos del arduino
+           
+            //NOTA envio la letra e ya que el sketch esta configurado para funcionar cuando
+            //recibe esta letra.
+            dataToSend = new Java.Lang.String("e");
+            writeData(dataToSend);
+        }
+
+
+        //Evento para inicializar el hilo que escuchara las peticiones del bluetooth
+      
+
+
+
+        //Metodo de envio de datos la bluetooth
+        private void writeData(Java.Lang.String data)
+        {
+            //Extraemos el stream de salida
+            try
+            {
+                outStream = btSocket.OutputStream;
+            }
+            catch (System.Exception e)
+            {
+                System.Console.WriteLine("Error al enviar" + e.Message);
+            }
+
+            //creamos el string que enviaremos
+            Java.Lang.String message = data;
+
+            //lo convertimos en bytes
+            byte[] msgBuffer = message.GetBytes();
+
+            try
+            {
+                //Escribimos en el buffer el arreglo que acabamos de generar
+                outStream.Write(msgBuffer, 0, msgBuffer.Length);
+            }
+            catch (System.Exception e)
+            {
+                System.Console.WriteLine("Error al enviar" + e.Message);
+            }
+        }
+
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}

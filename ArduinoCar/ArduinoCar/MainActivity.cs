c2ab03c1eb16038398b172ece0301bbf753d1a21
ronald mirabal <ronaldmirabal@outlook.es>
2019-06-21 12:22:48 -0400

    ..

--- ArduinoCar/ArduinoCar/MainActivity.cs
+++ ArduinoCar/ArduinoCar/MainActivity.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace ArduinoCar
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "Tablet", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
 
